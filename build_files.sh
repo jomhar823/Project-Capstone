33aadb9c73aec7e06039a13af2b7d811d837c778
Thea Clarisse P. Delos Santos <theadelossantos08@gmail.com>
2023-11-22 00:59:59 +0800

    upd

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,6 @@
 source venv/Scripts/activate
 
-python -m pip install -r requirements.txt
-
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput --clear
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
+python3.11 manage.py collectstatic --noinput --clear
